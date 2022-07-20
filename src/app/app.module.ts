022f9f9adee840ddae8750b0bb48fa6a053f0bb7
hayteam5 <hayteam797@gmail.Com>
2022-07-20 14:54:23 +0200

    finish of directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 import { Style2Directive } from './directives/style2.directive';
+import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     Style2Directive,
+    IfnotDirective,
   ],
   imports: [
     FormsModule,
--- src/app/directives/style2.directive.ts
+++ src/app/directives/style2.directive.ts
@@ -1,20 +1,34 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 import {Event} from "@angular/router";
 
 @Directive({
   selector: '[appStyle2]'
 })
 export class Style2Directive {
-  constructor(private el: ElementRef, private r: Renderer2) {
+  @Input()
+  dStyles!: { color?: string; border?: string; borderRadius: string; fontWeight?: string; };
 
+  constructor(private el: ElementRef, private r: Renderer2) {
   }
+  @HostBinding('style.fontStyle') elFontStyle: any = null
+
+
   @HostListener('click', ['$event.target']) onClick(event: Event) {
     console.log(event)
   }
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+    this.elFontStyle = 'italic'
+    this.r.setStyle(this.el.nativeElement, 'color', this.dStyles.color)
+    this.r.setStyle(this.el.nativeElement, 'font-weight', this.dStyles.fontWeight)
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
+    this.r.setStyle(this.el.nativeElement, 'border-radius', this.dStyles.borderRadius)
+
   }
   @HostListener('mouseleave') onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'black')
+    this.elFontStyle = null
+    this.r.setStyle(this.el.nativeElement, 'color', null)
+    this.r.setStyle(this.el.nativeElement, 'font-weight', null)
+    this.r.setStyle(this.el.nativeElement, 'border', null)
+    this.r.setStyle(this.el.nativeElement, 'border-radius', null)
   }
 }
